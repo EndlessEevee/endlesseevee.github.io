181a0fe2ee3b27d057da1d34b84c57cbca969d23
Unknown <53575465+EndlessEevee@users.noreply.github.com>
2024-05-03 14:09:20 -0700

    Shop Testing

---
+++ authentication.js
@@ -0,0 +1,34 @@
+// Set your secret key. Remember to switch to your live secret key in production.
+// See your keys here: https://dashboard.stripe.com/apikeys
+const stripe = require('stripe')('sk_test_51PBMFWCZYaFRUYezJHsiqgp6BHarFfhZjlNkDT8MNnxSOpCe3pDR427dyqYJMmEIF5JRe6aQc16KxHK3euoMZ9de00r03MbmAR');
+(async () => {
+    const product = await stripe.products.create({
+    name: 'Team Hoodie',
+    description: "Team Paradise's team hoodie for the FRC Crescendo Season.",
+    active: "true",
+    price_data: {
+        currency: 'usd',
+        active: true,
+        billing_scheme: 'per_unit',
+        unit_amount: 4500, 
+    },
+    });
+    console.log(product);
+})()
+
+// Set your secret key. Remember to switch to your live secret key in production.
+// See your keys here: https://dashboard.stripe.com/apikeys
+const stripe = require('stripe')('sk_test123');
+
+const product = await stripe.products.create({
+  description: 'Team Paradise\'s team hoodie for the FRC Crescendo Season.',
+  name: 'Team Hoodie',
+  active: true,
+  shippable: false,
+  statement_descriptor: 'Team Paradise',
+  type: 'good',
+  default_price_data: {
+    currency: 'usd',
+    unit_amount: 4500,
+  },
+});
